9834804941a8e163b55bf7d0ae1d6cf68bcf8c4d
Jamie Maguire <jamieolivermaguire@googlemail.com>
2024-10-24 18:07:30 +0100

    setup and teardown test files for integration tests

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,4 +1,4 @@
-import { describe, it, expect, beforeAll, afterAll } from 'vitest';
+import { describe, it, expect, beforeAll, beforeEach, afterEach, afterAll } from 'vitest';
 import { exec } from 'child_process';
 import util from 'util';
 import fs from 'fs/promises';
@@ -7,6 +7,16 @@ import path from 'path';
 const execPromise = util.promisify(exec);
 
 describe('BundleSizeAnalyser Integration Test', () => {
+  beforeEach(async () => {
+    const scriptPath = path.resolve(__dirname, './genfiles.js');
+    await execPromise(`node ${scriptPath}`);
+  });
+
+  afterEach(async () => {
+    const testEnvPath = path.resolve(__dirname, './testEnv');
+    await fs.rm(testEnvPath, { recursive: true, force: true });
+  });
+
   it('should correctly report the size of JS, gzipped, and brotli compressed files', async () => {
     const toolPath = path.resolve(__dirname, '../index.js');
     const configPath = path.join(__dirname, 'compsizer.config.json');
